90a5e629744e8c294f3daec179db403ba4bc25eb
Glitch (ripe-mango) <none>
2018-09-19 03:43:37 +0000

    🔥🌁 Checkpoint ./controllers/auth.js:43428/479

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,15 +10,20 @@ const handlers = {
   login: async props => { console.log('handle login'); return { res: 123 } },
 }
 
+const encodeResponse = props => Object.keys(props)
+.map(key => `"${key}" "${props[key]}"`)
+.join('\n')
+
 router.get('/', async (ctx, next) => {
-  console.log(`GET ${prefix}, headers`, ctx.headers)
+  // console.log(`GET ${prefix}, headers`, ctx.headers)
   console.log('type', ctx.query.type)
   const { type } = ctx.query
   if (!handlers[type]) {
-    throw
-  switch (type) {
-    case 'login': 
-  ctx.body = 'this is wip...'
+    throw new Error('No handler for request type')
+  }
+  const response = await handlers[type]()
+  console.log('response', response)
+  ctx.body = encodeResponse(response)
   next();
 });
 
