a2ec9167c5ee75a0ffcea245312913d12527c1f0
Glitch (ripe-mango) <none>
2018-09-19 06:56:51 +0000

    🚋💦 Checkpoint ./controllers/auth/logout.js:43428/498

---
+++ controllers/auth/logout.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+const debug = require('debug')('captive-portal:logout')
+const calcRequestAuthenticator = require('../../lib/ra')
+
+module.exports = async props => {
+  
+  const { ra, mac, node, download, upload, seconds, ipv4, session } = props
+  debug('in', props)
+  assert.ok(ra, 'ra required')
+  assert.ok(session, 'session required')
+  
+  // TODO: log accounting data
+  
+  const CODE = 'OK'
+  return {
+    CODE,
+    RA: calcRequestAuthenticator({ ra, code: CODE })
+  }
+}
