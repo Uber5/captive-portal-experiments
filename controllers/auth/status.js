ea1f379702225d6a62b304a4630efcc49ae021fe
Glitch (ripe-mango) <none>
2018-09-19 06:24:39 +0000

    🍷🌖 Checkpoint ./controllers/auth/status.js:43428/29

--- controllers/auth/status.js
+++ controllers/auth/status.js
@@ -2,7 +2,7 @@ const calcRequestAuthenticator = require('../../lib/ra')
 
 module.exports = async props => {
   const { ra, mac, node, ipv4, session } = props
-  console.log('status, requesting access, mac')
+  console.log('status, requesting access: mac, session', mac, session)
   
   // check if this session / mac are still valid or known?
   const code = 'REJECT'
